18a7a3017fa94912b83dfb486f55b586d342a968
Fawzy <mohamed.fawzy7996@gmail.com>
2022-04-16 15:13:10 +0000

    connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config({
+  path: `${__dirname}/../.env`,
+});
+import './config/db';
 
 const app = express();
-
 app.listen(3000, () => {
   console.log('running on port 3000');
 });
